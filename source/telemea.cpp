69fc7385d3b9e675d3b9a94a6a868f76956e3807
pachuco <ifail@life.net>
2025-05-30 03:16:26 +0300

    Some work on getting telemetry ready.

---
+++ source/telemea.cpp
@@ -0,0 +1,133 @@
+extern "C" {
+	#include "serial.h"
+}
+
+#define STR_STARTSWITH(STR, PREFIX) (strncmp(PREFIX, STR, strlen(PREFIX)) == 0)
+
+static int serialnum_open = -1;
+
+
+
+
+
+
+
+
+
+
+static struct {
+	// from perspective of environment, outside the suit
+	/*
+	float environment_gravity;
+	float environment_temperature;
+	float environment_pressure;
+	int   environment_star_class;
+	float environment_star_mass;
+	int   environment_planet_class;
+	int   environment_planet_mass;
+	*/
+	
+	// from perspective of suit/astronaut
+	float astronaut_tiredness;    // tiredness
+	float astronaut_gravity;      // pp_gravity
+	float astronaut_temperature;  // pp_temp
+	float astronaut_pressure;     // pp_pressure
+	float astronaut_pulse;        // pp_pulse
+	
+	// miscelaneous perceptual
+	/*
+		- is sun visible?
+		- what angle are you facintg sun at?
+		- how much space does it take on you FOV?
+		- is sun's turbulent surface visible?
+		- colours/palette
+		-
+	*/
+} teldat;
+
+
+int telemetry_startup_and_init_serial(char *serialconfpath)
+{
+	char linebuf[256] = {0};
+	int sret = 0;
+	int num_parsestage = 0;
+	FILE *fconf = NULL;
+	
+	memset(&teldat, 0, sizeof(teldat));
+	
+	if ((fconf = fopen(serialconfpath, "r")) == NULL) return -20;
+	
+	// lazy ini parser
+	static char *keynames[] = {
+		"",
+		"port_number=",
+		"data_rate=",
+		"data_bits=",
+		"parity=",
+		"stop_bits=",
+		"handshake="
+	};
+	int val_numport=0; long val_bps=0; int val_databits=0; char val_parity=0; int val_stopbits=0; int val_handshake=0;
+	while (fgets(linebuf, sizeof(linebuf), fconf) != NULL) {
+		if (num_parsestage == 0) { // find ini section
+			if (strcmp(linebuf, "[serial]\n") == 0)
+				num_parsestage++;
+		} else if (num_parsestage == 7) { // done!
+			break;
+		} else { // find key=value
+			if (STR_STARTSWITH(linebuf, keynames[num_parsestage])) {
+				int len_keyname = strlen(keynames[num_parsestage]);
+				
+				switch (num_parsestage) {
+					case 1: num_parsestage++; val_numport   = atoi(linebuf + len_keyname) - 1; break; // see serial.h
+					case 2: num_parsestage++; val_bps       = atol(linebuf + len_keyname); break;
+					case 3: num_parsestage++; val_databits  = atoi(linebuf + len_keyname); break;
+					case 4: num_parsestage++; val_parity    = linebuf[len_keyname]; break;
+					case 5: num_parsestage++; val_stopbits  = atoi(linebuf + len_keyname); break;
+					case 6: {
+						if (strcmp(linebuf + len_keyname, "none") == 0) {
+							num_parsestage++;
+							val_handshake = SER_HANDSHAKING_NONE;
+						} else if (strcmp(linebuf + len_keyname, "xonxoff") == 0) {
+							num_parsestage++;
+							val_handshake = SER_HANDSHAKING_XONXOFF;
+						} else if (strcmp(linebuf + len_keyname, "rtscts") == 0) {
+							num_parsestage++;
+							val_handshake = SER_HANDSHAKING_RTSCTS;
+						} else if (strcmp(linebuf + len_keyname, "dtrdts") == 0) {
+							num_parsestage++;
+							val_handshake = SER_HANDSHAKING_DTRDSR;
+						}
+					} break;
+				}
+			}
+		}
+	}
+	fclose(fconf);
+	
+	if (num_parsestage != 7)
+		return -21;
+	if ((sret = serial_open(val_numport, val_bps, val_databits, val_parity, val_stopbits, val_handshake)) != SER_SUCCESS)
+		return sret; // 0 to -16
+	serialnum_open = val_numport;
+	
+//char helloMsg[] = "Hello from noctis.\n"; serial_write(val_numport, helloMsg, sizeof(helloMsg));
+	return 0;
+}
+
+void telemetry_shutdown()
+{
+	int snum = serialnum_open;
+	
+	if (snum > 0) {
+		serialnum_open = -1;
+		serial_close(snum);
+		memset(&teldat, 0, sizeof(teldat));
+	}
+}
+/*
+void telemetry_update_astronaut_ppstuff(float gravity, float temperature, float pressure, float pulse)
+{
+	
+}
+*/
---
+++ source/telemea.h
@@ -0,0 +1,7 @@
+#ifndef TELEMEA_HEADER
+#define TELEMEA_HEADER
+
+int telemetry_startup_and_init_serial(char *serialconfpath);
+void telemetry_shutdown();
+
+#endif
